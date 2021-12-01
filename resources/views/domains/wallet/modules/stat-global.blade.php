67eff998ae3fd380c67c5f3991090b6d2a45fcde
Lito <lito@eordes.com>
2021-12-01 23:43:04 +0100

    Updated columns order on wallet global stats

--- resources/views/domains/wallet/modules/stat-global.blade.php
+++ resources/views/domains/wallet/modules/stat-global.blade.php
@@ -1,10 +1,5 @@
 <div class="box whitespace-nowrap flex flex-wrap text-center p-1 sm:p-2">
     <div class="flex-1 p-2">
-        <div class="text-gray-600 text-xs">{{ __('wallet-stat-global.investment') }}</div>
-        <div class="font-bold"><a href="{{ route('user.update') }}">@number($investment)</a></div>
-    </div>
-
-    <div class="flex-1 p-2">
         <div class="text-gray-600 text-xs">{{ __('wallet-stat-global.current_value') }}</div>
         <div class="font-bold">@number($current_value)</div>
     </div>
@@ -17,6 +12,11 @@
     <div class="flex-1 p-2 {{ ($sell_stop_min_value && ($current_value >= $sell_stop_min_value)) ? 'text-theme-10' : '' }}">
         <div class="text-gray-600 text-xs">{{ __('wallet-stat-global.sell_stop_min_value') }}</div>
         <div class="font-bold">@number($sell_stop_min_value)</div>
+    </div>
+
+    <div class="flex-1 p-2">
+        <div class="text-gray-600 text-xs">{{ __('wallet-stat-global.investment') }}</div>
+        <div class="font-bold"><a href="{{ route('user.update') }}">@number($investment)</a></div>
     </div>
 
     <div class="flex-1 p-2 {{ ($result >= 0) ? 'text-theme-10' : 'text-theme-24' }}">
