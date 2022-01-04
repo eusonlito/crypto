5dc99ca4a12dd5201c8c8ca3bbb28b8aa6ffd8c5
Lito <lito@eordes.com>
2022-01-04 20:42:07 +0100

    Minor Wallet Percent Calculator Fix

--- resources/views/domains/wallet/percent.blade.php
+++ resources/views/domains/wallet/percent.blade.php
@@ -429,33 +429,33 @@ charts.push({
 <div class="box p-5 mt-5">
     <div class="lg:flex">
         <div class="flex-auto p-2">
-            <label for="wallet-wallet_current_amount" class="form-label">{{ __('wallet-percent.wallet_start_amount') }}</label>
-            <input type="text" class="form-control form-control-lg" id="wallet-wallet_current_amount" value="@numberString($row->amount)" readonly />
+            <label for="wallet-wallet_start_amount" class="form-label">{{ __('wallet-percent.wallet_start_amount') }}</label>
+            <input type="text" class="form-control form-control-lg" id="wallet-wallet_start_amount" value="@numberString($REQUEST->input('amount'))" readonly />
         </div>
 
         <div class="flex-auto p-2">
-            <label for="wallet-wallet_current_amount" class="form-label">{{ __('wallet-percent.wallet_end_amount') }}</label>
-            <input type="text" class="form-control form-control-lg" id="wallet-wallet_current_amount" value="@numberString($result->amount)" readonly />
+            <label for="wallet-wallet_end_amount" class="form-label">{{ __('wallet-percent.wallet_end_amount') }}</label>
+            <input type="text" class="form-control form-control-lg" id="wallet-wallet_end_amount" value="@numberString($result->amount)" readonly />
         </div>
 
         <div class="flex-auto p-2">
-            <label for="wallet-wallet_current_exchange" class="form-label">{{ __('wallet-percent.wallet_start_exchange') }}</label>
-            <input type="text" class="form-control form-control-lg" id="wallet-wallet_current_exchange" value="@numberString($row->buy_exchange)" readonly />
+            <label for="wallet-wallet_start_exchange" class="form-label">{{ __('wallet-percent.wallet_start_exchange') }}</label>
+            <input type="text" class="form-control form-control-lg" id="wallet-wallet_start_exchange" value="@numberString($REQUEST->input('buy_exchange'))" readonly />
         </div>
 
         <div class="flex-auto p-2">
-            <label for="wallet-wallet_current_exchange" class="form-label">{{ __('wallet-percent.wallet_end_exchange') }}</label>
-            <input type="text" class="form-control form-control-lg" id="wallet-wallet_current_exchange" value="@numberString($result->buy_exchange)" readonly />
+            <label for="wallet-wallet_end_exchange" class="form-label">{{ __('wallet-percent.wallet_end_exchange') }}</label>
+            <input type="text" class="form-control form-control-lg" id="wallet-wallet_end_exchange" value="@numberString($result->buy_exchange)" readonly />
         </div>
 
         <div class="flex-auto p-2">
-            <label for="wallet-wallet_current_value" class="form-label">{{ __('wallet-percent.wallet_start_value') }}</label>
-            <input type="text" class="form-control form-control-lg" id="wallet-wallet_current_value" value="@number($row->buy_value, 2)" readonly />
+            <label for="wallet-wallet_start_value" class="form-label">{{ __('wallet-percent.wallet_start_value') }}</label>
+            <input type="text" class="form-control form-control-lg" id="wallet-wallet_start_value" value="@number($REQUEST->input('amount') * $REQUEST->input('buy_exchange'), 2)" readonly />
         </div>
 
         <div class="flex-auto p-2">
-            <label for="wallet-wallet_current_value" class="form-label">{{ __('wallet-percent.wallet_end_value') }}</label>
-            <input type="text" class="form-control form-control-lg" id="wallet-wallet_current_value" value="@number($result->buy_value, 2)" readonly />
+            <label for="wallet-wallet_end_value" class="form-label">{{ __('wallet-percent.wallet_end_value') }}</label>
+            <input type="text" class="form-control form-control-lg" id="wallet-wallet_end_value" value="@number($result->buy_value, 2)" readonly />
         </div>
 
         <div class="flex-auto p-2">
