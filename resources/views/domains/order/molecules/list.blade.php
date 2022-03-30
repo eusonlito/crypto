f2cbd9d49a6a5591fdb04230e5d86ca8faa03a8f
Lito <lito@eordes.com>
2022-03-30 22:06:41 +0200

    Updated order list columns

--- resources/views/domains/order/molecules/list.blade.php
+++ resources/views/domains/order/molecules/list.blade.php
@@ -7,6 +7,7 @@
                 <th class="text-center">{{ __('order-index.date') }}</th>
                 <th class="text-center">{{ __('order-index.product') }}</th>
                 <th class="text-center">{{ __('order-index.platform') }}</th>
+                <th class="text-center">{{ __('order-index.side') }}</th>
                 <th>{{ __('order-index.amount') }}</th>
                 <th>{{ __('order-index.price') }}</th>
 
@@ -23,7 +24,6 @@
                 <th>{{ __('order-index.fee') }}</th>
                 <th class="text-center">{{ __('order-index.type') }}</th>
                 <th class="text-center">{{ __('order-index.status') }}</th>
-                <th class="text-center">{{ __('order-index.side') }}</th>
                 <th class="text-center">{{ __('order-index.filled') }}</th>
             </tr>
         </thead>
@@ -35,6 +35,7 @@
                 <td><span class="block text-center whitespace-nowrap" title="{{ $row->created_at }}">@datetime($row->created_at)</span></td>
                 <td><a href="{{ route('exchange.detail', $row->product->id) }}" class="block text-center font-semibold whitespace-nowrap external">{{ $row->product->acronym }}</a></td>
                 <td><a href="{{ $row->platform->url.$row->product->code }}" rel="nofollow noopener noreferrer" target="_blank" class="block text-center font-semibold whitespace-nowrap external">{{ $row->platform->name }}</a></td>
+                <td><span class="block text-center">{{ ($row->side === 'sell') ? __('order-index.sell') : __('order-index.buy') }}</span></td>
                 <td><span class="block" title="{{ $row->amount }}">@number($row->amount)</span></td>
                 <td><span class="block" title="{{ $row->price }}">@number($row->price)</span></td>
 
@@ -55,7 +56,6 @@
                 <td><span class="block" title="{{ $row->fee }}">@number($row->fee)</span></td>
                 <td><span class="block text-center">{{ $row->type }}</span></td>
                 <td><span class="block text-center">{{ $row->status }}</span></td>
-                <td><span class="block text-center">{{ ($row->side === 'sell') ? __('order-index.sell') : __('order-index.buy') }}</span></td>
                 <td><span class="block text-center">@status($row->filled)</span></td>
             </tr>
 
