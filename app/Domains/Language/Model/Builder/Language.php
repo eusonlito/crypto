eebf6043031b41d496f30bfee6917b4d8a0b1593
Lito <lito@eordes.com>
2024-02-29 18:45:15 +0100

    Fixed User Set

--- app/Domains/Language/Model/Builder/Language.php
+++ app/Domains/Language/Model/Builder/Language.php
@@ -6,4 +6,11 @@ use App\Domains\Core\Model\Builder\BuilderAbstract;
 
 class Language extends BuilderAbstract
 {
+    /**
+     * @return self
+     */
+    public function selectSession(): self
+    {
+        return $this->select('id', 'code', 'name', 'locale');
+    }
 }
