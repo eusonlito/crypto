d4af7eb721a78a3af27dbc20a8b9068ddad5ae90
David Pérez Terol <davidtxnet@gmail.com>
2024-04-10 23:19:03 +0200

    Add languages to database seeder

--- database/Seeders/Database.php
+++ database/Seeders/Database.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Domains\Platform\Seeder\Platform as PlatformSeeder;
+use App\Domains\Language\Seeder\Language as LanguageSeeder;
 
 class Database extends Seeder
 {
@@ -15,6 +16,7 @@ class Database extends Seeder
         $time = time();
 
         $this->call(PlatformSeeder::class);
+        $this->call(LanguageSeeder::class);
 
         $this->command->info(sprintf('Seeding: Total Time %s seconds', time() - $time));
     }
