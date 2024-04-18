aa2975da025e65221cfb3343b3afcfbdb90fa27d
Lito <lito@eordes.com>
2024-04-18 17:52:15 +0200

    Updated assets

--- resources/views/assets/js/table-search.js
+++ resources/views/assets/js/table-search.js
@@ -1,40 +1,86 @@
-(function (cash) {
+(function () {
     'use strict';
 
-    const search = function (e) {
+    const search = function (e, element, $table, $trs) {
         if ((e.code === 'Enter') || (e.code === 'NumpadEnter')) {
             return e.preventDefault();
+        }
+
+        if (e.code === 'Escape') {
+            return searchReset(element, $table, $trs);
         }
 
         if (e.type !== 'input') {
             return;
         }
 
-        const $table = document.querySelector(this.dataset.tableSearch);
-        const $trs = cash($table).find('tbody > tr');
-        const value = this.value.toLowerCase().trim();
+        const value = element.value.toLowerCase().trim();
 
         if (!value.length) {
-            return searchReset($table, $trs);
+            return searchReset(element, $table, $trs);
         }
 
-        $trs.each(function () {
-            const content = (this.textContent || this.innerText)
-                .toLowerCase()
-                .replace(/(\n|\r|\s)+/gm, ' ');
+        $table.dispatchEvent(new CustomEvent('search', { detail: value }));
 
-            this.style.display = (content.indexOf(value) > -1) ? 'table-row' : 'none';
+        const search = value.split(' ');
+
+        $trs.forEach(element => {
+            element.dataset.tableSearchValue = element.dataset.tableSearchValue || contentValue(element);
+
+            const value = element.dataset.tableSearchValue;
+            let found = 0;
+
+            search.forEach(text => found += (value.indexOf(text) > -1) ? 1 : 0);
+
+            element.style.display = (found === search.length) ? 'table-row' : 'none';
         });
     };
 
-    const searchReset = function ($table, $trs) {
-        $trs.each(function () {
-            this.style.display = (this.dataset.tablePaginationHidden === 'true') ? 'none' : 'table-row';
+    const searchReset = function (element, $table, $trs) {
+        element.value = '';
+
+        $trs.forEach(element => {
+            element.style.display = (element.dataset.tablePaginationHidden === 'true') ? 'none' : 'table-row';
         });
 
-        $table.dispatchEvent(new CustomEvent('reset', { detail: 'search' }));
+        $table.dispatchEvent(new CustomEvent('search', { detail: '' }));
     };
 
-    cash('[data-table-search]').on('input', search);
-    cash('[data-table-search]').on('keydown', search);
-})(cash);
+    const contentValue = function ($tr) {
+        return ($tr.textContent || $tr.innerText)
+            .toLowerCase()
+            .replace(/(\n|\r|\s)+/gm, ' ')
+            .trim();
+    }
+
+    const load = function (element) {
+        if (element.dataset.tableSortLoaded || element.dataset.skipEventBinding) {
+            return;
+        }
+
+        element.dataset.tableSortLoaded = true;
+
+        const $table = document.querySelector(element.dataset.tableSearch);
+
+        if (!$table) {
+            return;
+        }
+
+        const $trs = $table.querySelectorAll('tbody > tr');
+
+        if (!$trs.length) {
+            return;
+        }
+
+        element.addEventListener('input', (e) => search(e, element, $table, $trs));
+        element.addEventListener('keydown', (e) => search(e, element, $table, $trs));
+    }
+
+    const init = function () {
+        document.querySelectorAll('[data-table-search]').forEach(load);
+    }
+
+    document.addEventListener('ajax', init);
+
+    init();
+})();
